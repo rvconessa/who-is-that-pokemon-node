056793a6436e6cb6e81683a33e87c1847961f264
Renan <rvichetti@artit.com.br>
2018-11-21 16:22:19 -0200

    Ajusts function

--- src/ultils/images.js
+++ src/ultils/images.js
@@ -1,17 +1,17 @@
 const jimp = require('jimp');
 
 const createResizeImageBase64 = async (url) => {
-    const image = await jimp.read(url);
+    let image = await jimp.read(url);
     await image.resize(180, jimp.AUTO)
-    const convert64 = image.getBase64Async(jimp.AUTO)
+    let convert64 = image.getBase64Async(jimp.AUTO)
 
     return convert64;
 }
 
 const createBlackImageBase64 = async (url) => {
-    const image = await jimp.read(url);
+    let image = await jimp.read(url);
     await image.color([{ apply: 'darken', params: [100] }]).resize(180, jimp.AUTO)
-    const convert64 = image.getBase64Async(jimp.AUTO)
+    let convert64 = image.getBase64Async(jimp.AUTO)
 
     return convert64;
 }
