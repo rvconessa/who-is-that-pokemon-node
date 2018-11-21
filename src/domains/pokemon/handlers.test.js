5d80fb218111bdabd3b1daf4434c46f72a4e0025
Renan <rvichetti@artit.com.br>
2018-11-21 15:57:54 -0200

    Add test checkPokemon

--- src/domains/pokemon/handlers.test.js
+++ src/domains/pokemon/handlers.test.js
@@ -33,4 +33,24 @@ describe('Domains/Pokemon/Handlers', () => {
         expect(body[0].pokemonImage).equal64();
       });
     });
+    
+    describe('checkPokemon', () => {
+        const api = require('../../api');
+      it('check pokemon name is correct', async () => {
+        const server = await api({ port: null });
+
+        const { body, status } = await request(server)
+            .post('/pokemon')
+            .send( {
+                "name": "bibarel",
+                "encryptName": "7f75a1353719f3b7725a1cbf63014c76"
+            })
+            .set('Accept', 'application/json')
+    
+        expect(status).toBe(200);
+        expect(body[0]).toHaveProperty('name');
+        expect(body[0]).toHaveProperty('pokemonImage');
+        expect(body[0].pokemonImage).equal64();
+      });
+    });
   })
